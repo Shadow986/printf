9e5b192f6585550bc016104fc4f536737edf7aaa
Shadow986 <91501629+Shadow986@users.noreply.github.com>
2023-09-18 11:19:32 +0200

    Update print_handle_length_modifiers.c

--- print_handle_length_modifiers.c
+++ print_handle_length_modifiers.c
@@ -2,45 +2,45 @@
 #include "main.h"
 
 /**
- * print_int - a function that prints an integer.
+ * get_int - a function that prints an integer.
  * @l: va_list parameter (unused)
  * @f: Pointer to flags_t structure (unused)
  * Return: Length of printed characters or -1 on error
  */
-int print_int(va_list l, flags_t *f)
+int get_int(va_list l, flags_t *f)
 {
 	return (0);
 }
 
 /**
- * print_uint - a function that prints an unsigned integer.
+ * get_uint - a function that prints an unsigned integer.
  * @l: va_list parameter (unused)
  * @f: Pointer to flags_t structure (unused)
  * Return: Length of printed characters or -1 on error
  */
-int print_uint(va_list l, flags_t *f)
+int get_uint(va_list l, flags_t *f)
 {
 	return (0);
 }
 
 /**
- * print_octal - a function that prints a number in octal format.
+ * get_octal - a function that prints a number in octal format.
  * @l: va_list parameter (unused)
  * @f: Pointer to flags_t structure (unused)
  * Return: Length of printed characters or -1 on error
  */
-int print_octal(va_list l, flags_t *f)
+int get_octal(va_list l, flags_t *f)
 {
 	return (0);
 }
 
 /**
- * print_hex - a function that prints a number in hexadecimal format.
+ * get_hex - a function that prints a number in hexadecimal format.
  * @l: va_list parameter (unused)
  * @f: Pointer to flags_t structure (unused)
  * Return: Length of printed characters or -1 on error
  */
-int print_hex(va_list l, flags_t *f)
+int get_hex(va_list l, flags_t *f)
 {
 	return (0);
 }
@@ -54,7 +54,7 @@ int print_hex(va_list l, flags_t *f)
  */
 int handle_length_modifiers(va_list l, flags_t *f)
 {
-	char c = *(f->format);
+	char c = *(f->fo);
 	int length = 0;
 
 	switch (c)
@@ -68,23 +68,23 @@ int handle_length_modifiers(va_list l, flags_t *f)
 		default:
 			return (-1);
 	}
-	f->format++;
-	c = *(f->format);
+	f->fo++;
+	c = *(f->fo);
 	switch (c)
 	{
 		case 'd':
 		case 'i':
-			length = print_int(l, f);
+			length = get_int(l, f);
 			break;
 		case 'u':
-			length = print_uint(l, f);
+			length = get_uint(l, f);
 			break;
 		case 'o':
-			length = print_octal(l, f);
+			length = get_octal(l, f);
 			break;
 		case 'x':
 		case 'X':
-			length = print_hex(l, f);
+			length = get_hex(l, f);
 			break;
 		default:
 			return (-1);
