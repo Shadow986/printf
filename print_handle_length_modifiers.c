f964801d7329c846c32708ccf37d61056208768a
Shadow986 <archie986jr@gmail.com>
2023-09-18 09:21:02 -0700

    update

--- print_handle_length_modifiers.c
+++ print_handle_length_modifiers.c
@@ -2,80 +2,6 @@
 #include <stdio.h>
 #include "main.h"
 
-/**
- * get_printf - a function that implements a custom printf behavior.
- * @l: va_list parameter
- * @f: Pointer to flags_t structure
- * Return: Length of printed characters or -1 on error
- */
-int get_printf(va_list l, flags_t *f)
-{
-	const char *fo = f->fo;
-	va_list args;
-	va_copy(args, l);
-	int width = 0;
-	int field_width = 0;
-	int precision = 0;
-	int precision_flag = 0;
-	int length = 0;
-
-	for (; *fo; fo++)
-	{
-		if (*fo == '%')
-		{
-			fo++;
-			if (*fo >= '0' && *fo <= '9')
-			{
-				width = 0;
-				for (; *fo >= '0' && *fo <= '9'; fo++)
-				{
-					width = width * 10 + (*fo - '0');
-				}
-			}
-			if (*fo == '.')
-			{
-				fo++;
-				precision_flag = 1;
-				if (*fo >= '0' && *fo <= '9')
-				{
-					precision = 0;
-					for (; *fo >= '0' && *fo <= '9'; fo++)
-					{
-						precision = precision * 10 + (*fo - '0');
-					}
-				}
-			}
-			switch (*fo)
-			{
-				case 'd':
-				case 'i':
-					field_width = width - precision_flag - precision;
-					length += printf("%*.*d", field_width, precision, va_arg(args, int));
-					break;
-				case 'u':
-					field_width = width - precision_flag - precision;
-					length += printf("%*.*u", field_width, precision, va_arg(args, unsigned int));
-					break;
-				case 'o':
-					break;
-				case 'x':
-				case 'X':
-					break;
-				default:
-					putchar(*fo);
-					length++;
-					break;
-			}
-		}
-		else
-		{
-			putchar(*fo);
-			length++;
-		}
-	}
-	va_end(args);
-	return length;
-}
 
 /**
  * get_int - a function that prints an integer using get_printf.
@@ -85,18 +11,7 @@ int get_printf(va_list l, flags_t *f)
  */
 int get_int(va_list l, flags_t *f)
 {
-	return get_printf(l, f);
-}
-
-/**
- * get_uint - a function that prints an unsigned integer using get_printf.
- * @l: va_list parameter
- * @f: Pointer to flags_t structure
- * Return: Length of printed characters or -1 on error
- */
-int get_uint(va_list l, flags_t *f)
-{
-	return get_printf(l, f);
+	return (get_printf(l, f));
 }
 
 /**
@@ -111,7 +26,7 @@ int get_octal(va_list l, flags_t *f)
 	unsigned int remainder;
 	int x = 0;
 	char octal_num[32] = {0};
-	
+
 	while (num != 0)
 	{
 		remainder = num % 8;
@@ -122,8 +37,7 @@ int get_octal(va_list l, flags_t *f)
 	{
 		putchar(octal_num[x]);
 	}
-
-    return (x);
+	return (x);
 }
 
 /**
@@ -149,8 +63,7 @@ int print_hex(va_list l, flags_t *f)
 	{
 		putchar(hex_num[x]);
 	}
-
-    return (x);
+	return (x);
 }
 
 /**
@@ -198,13 +111,13 @@ int handle_length_modifiers(va_list l, flags_t *f)
 		default:
 			return (-1);
 	}
-
-    return (length);
+	return (length);
 }
 
-int main()
+int main(void)
 {
 	int num = 42;
+
 	printf("Print integer: %d\n", num);
 	return (0);
 }
