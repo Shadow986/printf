f55d2ab5e9cd71b6b9eafcb8994dd58fa2073f71
Ben Vilakazi <xolanithegeengubane@gmail.com>
2023-09-19 18:10:18 +0200

    print hexa upper

--- primary_func.c
+++ primary_func.c
@@ -91,3 +91,39 @@ int flags, int width, int precision, int size)
 return (print_hexa(types, "0123456789ABCDEF", buffer,
 flags, 'X', width, precision, size));
 }
+/**
+ * print_hexa - print a hex num in upper or lower
+ * @types: list of args
+ * @map_to: arr of val
+ * @buffer: buffer arr
+ * @flags:  calc flags
+ * @flag_ch: calc flags
+ * @width: width
+ * @precision: prec specs
+ * @size: size specs
+ * Return: Number of chars printed
+ */
+int print_hexa(va_list types, char map_to[], char buffer[],
+int flags, char flag_ch, int width, int precision, int size)
+{
+int i = BUFF_SIZE - 2;
+unsigned long int num = va_arg(types, unsigned long int);
+unsigned long int init_num = num;
+UNUSED(width);
+num = convert_size_unsgnd(num, size);
+if (num == 0)
+buffer[i--] = '0';
+buffer[BUFF_SIZE - 1] = '\0';
+while (num > 0)
+{
+buffer[i--] = map_to[num % 16];
+num /= 16;
+}
+if (flags & F_HASH && init_num != 0)
+{
+buffer[i--] = flag_ch;
+buffer[i--] = '0';
+}
+i++;
+return (write_unsgnd(0, i, buffer, flags, width, precision, size));
+}
