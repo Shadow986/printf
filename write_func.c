109fa3a569150c3ef0abe1e2033517ae4c71a205
Ben Vilakazi <xolanithegeengubane@gmail.com>
2023-09-19 16:56:50 +0200

    write pointer

--- write_func.c
+++ write_func.c
@@ -226,3 +226,99 @@ return (write(1, &buffer[ind], length));
 
 }
 
+/**
+ * write_pointer - Write a memloc
+ * @buffer: arr of chars
+ * @ind: At what index does the num begin in the buffer?
+ * @length: len of num
+ * @width: width specs
+ * @flags: Flags specs
+ * @padd: char reping padding
+ * @extra_c: char reping extra char
+ * @padd_start: At which index should the padding begin?
+ * Return: num of chars.
+ */
+
+int write_pointer(char buffer[], int ind, int length,
+
+int width, int flags, char padd, char extra_c, int padd_start)
+
+{
+
+int i;
+
+
+
+if (width > length)
+
+{
+
+for (i = 3; i < width - length + 3; i++)
+
+buffer[i] = padd;
+
+buffer[i] = '\0';
+
+if (flags & F_MINUS && padd == ' ')
+
+{
+
+buffer[--ind] = 'x';
+
+buffer[--ind] = '0';
+
+if (extra_c)
+
+buffer[--ind] = extra_c;
+
+return (write(1, &buffer[ind], length) + write(1, &buffer[3], i - 3));
+
+}
+
+else if (!(flags & F_MINUS) && padd == ' ')
+
+{
+
+buffer[--ind] = 'x';
+
+buffer[--ind] = '0';
+
+if (extra_c)
+
+buffer[--ind] = extra_c;
+
+return (write(1, &buffer[3], i - 3) + write(1, &buffer[ind], length));
+
+}
+
+else if (!(flags & F_MINUS) && padd == '0')
+
+{
+
+if (extra_c)
+
+buffer[--padd_start] = extra_c;
+
+buffer[1] = '0';
+
+buffer[2] = 'x';
+
+return (write(1, &buffer[padd_start], i - padd_start) +
+
+write(1, &buffer[ind], length - (1 - padd_start) - 2));
+
+}
+
+}
+
+buffer[--ind] = 'x';
+
+buffer[--ind] = '0';
+
+if (extra_c)
+
+buffer[--ind] = extra_c;
+
+return (write(1, &buffer[ind], BUFF_SIZE - ind - 1));
+
+}
