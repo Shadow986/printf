ffd951e5809a87ddcaa11682b8fc8a59d95a1585
Ben Vilakazi <xolanithegeengubane@gmail.com>
2023-09-20 00:02:08 +0200

    main.h

--- main.h
+++ main.h
@@ -72,3 +72,23 @@ int _printf(const char *format, ...);
 int handle_print(const char *fmt, int *i,
 
 va_list list, char buffer[], int flags, int width, int precision, int size);
+
+
+
+/****************** FUNCTIONS ******************/
+
+
+
+/* Funtions to print chars and strings */
+
+int print_char(va_list types, char buffer[],
+
+int flags, int width, int precision, int size);
+
+int print_string(va_list types, char buffer[],
+
+int flags, int width, int precision, int size);
+
+int print_percent(va_list types, char buffer[],
+
+int flags, int width, int precision, int size);
