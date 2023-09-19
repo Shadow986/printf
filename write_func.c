1d203d0ed52ff89162de1e0f9a18f07e53c52800
Ben Vilakazi <xolanithegeengubane@gmail.com>
2023-09-19 16:58:28 +0200

    final touch up

--- write_func.c
+++ write_func.c
@@ -72,6 +72,7 @@ return (write(1, &buffer[0], 1));
 
 }
 
+
 /**
  * write_number - prints a str
  * @is_negative: lista of args
@@ -121,6 +122,109 @@ extra_ch = ' ';
 return (write_num(ind, buffer, flags, width, precision,
 
 length, padd, extra_ch));
+
+}
+ 
+ /**
+ * write_num - writes a num using a bufffer
+ * @ind: The starting index of the number within the buffer.
+ * @buffer: buffer
+ * @flags: flags
+ * @width: width
+ * @prec: preci specs
+ * @length: num len
+ * @padd: padding char
+ * @extra_c: xtra char
+ * Return: num of chars.
+ */
+
+int write_num(int ind, char buffer[],
+
+int flags, int width, int prec,
+
+int length, char padd, char extra_c)
+
+{
+
+int i, padd_start = 1;
+
+
+
+if (prec == 0 && ind == BUFF_SIZE - 2 && buffer[ind] == '0' && width == 0)
+
+return (0); 
+
+if (prec == 0 && ind == BUFF_SIZE - 2 && buffer[ind] == '0')
+
+buffer[ind] = padd = ' '; 
+
+if (prec > 0 && prec < length)
+
+padd = ' ';
+
+while (prec > length)
+
+buffer[--ind] = '0', length++;
+
+if (extra_c != 0)
+
+length++;
+
+if (width > length)
+
+{
+
+for (i = 1; i < width - length + 1; i++)
+
+buffer[i] = padd;
+
+buffer[i] = '\0';
+
+if (flags & F_MINUS && padd == ' ')
+
+{
+
+if (extra_c)
+
+buffer[--ind] = extra_c;
+
+return (write(1, &buffer[ind], length) + write(1, &buffer[1], i - 1));
+
+}
+
+else if (!(flags & F_MINUS) && padd == ' ')
+
+{
+
+if (extra_c)
+
+buffer[--ind] = extra_c;
+
+return (write(1, &buffer[1], i - 1) + write(1, &buffer[ind], length));
+
+}
+
+else if (!(flags & F_MINUS) && padd == '0')
+
+{
+
+if (extra_c)
+
+buffer[--padd_start] = extra_c;
+
+return (write(1, &buffer[padd_start], i - padd_start) +
+
+write(1, &buffer[ind], length - (1 - padd_start)));
+
+}
+
+}
+
+if (extra_c)
+
+buffer[--ind] = extra_c;
+
+return (write(1, &buffer[ind], length));
 
 }
 
