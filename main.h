61216469ce16202401ca05fa3c008074e11dc5f1
Ben Vilakazi <xolanithegeengubane@gmail.com>
2023-09-20 00:10:06 +0200

    func print

--- main.h
+++ main.h
@@ -134,3 +134,12 @@ char buffer[], int flags, char flag_ch, int width, int precision, int size);
 int print_non_printable(va_list types, char buffer[],
 
 int flags, int width, int precision, int size);
+
+
+
+/* Funcion to print memory address */
+
+int print_pointer(va_list types, char buffer[],
+
+int flags, int width, int precision, int size);
+
