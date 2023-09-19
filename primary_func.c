69da7cd09a411c49d067daf3f52b25e3073164f5
Ben Vilakazi <xolanithegeengubane@gmail.com>
2023-09-19 17:48:18 +0200

    print octal

--- primary_func.c
+++ primary_func.c
@@ -27,3 +27,35 @@ num /= 10;
 i++;
 return (write_unsgnd(0, i, buffer, flags, width, precision, size));
 }
+
+/**
+ * print_octal - print an unsigned int in octal notation
+ * @types: lista of args
+ * @buffer: buffer arr
+ * @flags:  calc flags
+ * @width: width
+ * @precision: prec specs
+ * @size: size specs
+ * Return: num of chars
+ */
+int print_octal(va_list types, char buffer[],
+int flags, int width, int precision, int size)
+{
+int i = BUFF_SIZE - 2;
+unsigned long int num = va_arg(types, unsigned long int);
+unsigned long int init_num = num;
+UNUSED(width);
+num = convert_size_unsgnd(num, size);
+if (num == 0)
+buffer[i--] = '0';
+buffer[BUFF_SIZE - 1] = '\0';
+while (num > 0)
+{
+buffer[i--] = (num % 8) + '0';
+num /= 8;
+}
+if (flags & F_HASH && init_num != 0)
+buffer[i--] = '0';
+i++;
+return (write_unsgnd(0, i, buffer, flags, width, precision, size));
+}
